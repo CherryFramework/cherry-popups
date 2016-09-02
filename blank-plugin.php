2f02c16c8f513fa21ceeae205884523a1661a970
shin <shin@templatemonster.me>
2016-09-02 14:38:41 +0300

    add: init

---
+++ blank-plugin.php
@@ -0,0 +1,306 @@
+<?php
+/**
+ * Plugin Name: Blank Plugin
+ * Plugin URI:  http://www.cherryframework.com/
+ * Description: A plugin for WordPress.
+ * Version:     1.0.0
+ * Author:      Cherry Team
+ * Text Domain: blank-plugin
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: /languages
+ *
+ * @package Blank_Plugin
+ * @author  Cherry Team
+ * @version 1.0.0
+ * @license GPL-3.0+
+ * @copyright  2002-2016, Cherry Team
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die();
+}
+
+// If class `Blank_Plugin` doesn't exists yet.
+if ( ! class_exists( 'Blank_Plugin' ) ) {
+
+	/**
+	 * Sets up and initializes the Blank plugin.
+	 */
+	class Blank_Plugin {
+
+		/**
+		 * A reference to an instance of this class.
+		 *
+		 * @since 1.0.0
+		 * @access private
+		 * @var   object
+		 */
+		private static $instance = null;
+
+		/**
+		 * A reference to an instance of cherry framework core class.
+		 *
+		 * @since 1.0.0
+		 * @access private
+		 * @var   object
+		 */
+		private $core = null;
+
+		/**
+		 * Sets up needed actions/filters for the plugin to initialize.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function __construct() {
+			// Set the constants needed by the plugin.
+			$this->constants();
+
+			// Load the installer core.
+			add_action( 'after_setup_theme', require( trailingslashit( __DIR__ ) . 'cherry-framework/setup.php' ), 0 );
+
+			// Load the core functions/classes required by the rest of the plugin.
+			add_action( 'after_setup_theme', array( $this, 'get_core' ), 1 );
+
+			// Laad the modules.
+			add_action( 'after_setup_theme', array( 'Cherry_Core', 'load_all_modules' ), 2 );
+
+			// Initialization of modules.
+			add_action( 'after_setup_theme', array( $this, 'init_modules' ), 3 );
+
+			// Internationalize the text strings used.
+			add_action( 'plugins_loaded', array( $this, 'lang' ), 1 );
+
+			// Load the admin files.
+			add_action( 'plugins_loaded', array( $this, 'admin' ), 2 );
+
+			// Register public assets.
+			add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ), 10 );
+
+			// Load public-facing StyleSheets.
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 11 );
+
+			// Load public-facing JavaScripts.
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 12 );
+
+			// Register activation and deactivation hook.
+			register_activation_hook( __FILE__, array( $this, 'activation' ) );
+			register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
+		}
+
+		/**
+		 * Defines constants for the plugin.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function constants() {
+
+			/**
+			 * Set the version number of the plugin.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'BLANK_PLUGIN_VERSION', '1.0.0' );
+
+			/**
+			 * Set the slug of the plugin.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'BLANK_PLUGIN_SLUG', basename( dirname( __FILE__ ) ) );
+
+			/**
+			 * Set constant path to the plugin directory.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'BLANK_PLUGIN_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+			/**
+			 * Set constant path to the plugin URI.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'BLANK_PLUGIN_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+		}
+
+		/**
+		 * Loads the core functions. These files are needed before loading anything else in the
+		 * plugin because they have required functions for use.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @return object
+		 */
+		public function get_core() {
+
+			/**
+			 * Fires before loads the plugin's core.
+			 *
+			 * @since 1.0.0
+			 */
+			do_action( 'blank_plugin_core_before' );
+
+			global $chery_core_version;
+
+			if ( null !== $this->core ) {
+				return $this->core;
+			}
+
+			if ( 0 < sizeof( $chery_core_version ) ) {
+				$core_paths = array_values( $chery_core_version );
+				require_once( $core_paths[0] );
+			} else {
+				die( 'Class Cherry_Core not found' );
+			}
+
+			$this->core = new Cherry_Core( array(
+				'base_dir' => BLANK_PLUGIN_DIR . 'cherry-framework',
+				'base_url' => BLANK_PLUGIN_URI . 'cherry-framework',
+				'modules'  => array(
+					'cherry-js-core' => array(
+						'autoload' => true,
+					),
+					'cherry-toolkit' => array(
+						'autoload' => false,
+					),
+					'cherry-ui-elements' => array(
+						'autoload' => false,
+					),
+					'cherry-interface-builder' => array(
+						'autoload' => false,
+					),
+				),
+			) );
+
+			return $this->core;
+		}
+
+		/**
+		 * Run initialization of modules.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function init_modules() {
+			if ( is_admin() ) {
+				$this->get_core()->init_module( 'cherry-interface-builder', array() );
+			}
+		}
+
+		/**
+		 * Loads admin files.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function admin() {
+			if ( is_admin() ) {
+				require_once( BLANK_PLUGIN_DIR . 'includes/admin/class-plugin-admin.php' );
+			}
+		}
+
+		/**
+		 * Loads the translation files.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function lang() {
+			load_plugin_textdomain( 'blank-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+		}
+
+		/**
+		 * Register assets.
+		 *
+		 * @since 1.0.0
+		 */
+		public function register_assets() {
+			// Register stylesheets.
+			wp_register_style( 'blank-plugin', esc_url( BLANK_PLUGIN_URI . 'assets/css/min/blank-plugin.min.css' ), array(), BLANK_PLUGIN_VERSION, 'all' );
+
+			// Register JavaScripts.
+			wp_register_script( 'blank-plugin',esc_url( BLANK_PLUGIN_URI . 'assets/js/min/blank-plugin.min.js' ), array( 'cherry-js-core' ), BLANK_PLUGIN_VERSION, true );
+		}
+
+		/**
+		 * Enqueue public-facing stylesheets.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function enqueue_styles() {
+			wp_enqueue_style( 'blank-plugin' );
+		}
+
+		/**
+		 * Enqueue public-facing JavaScripts.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function enqueue_scripts() {
+			wp_enqueue_script( 'blank-plugin' );
+		}
+
+		/**
+		 * On plugin activation.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function activation() {}
+
+		/**
+		 * On plugin deactivation.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function deactivation() {}
+
+		/**
+		 * Returns the instance.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @return object
+		 */
+		public static function get_instance() {
+
+			// If the single instance hasn't been set, set it now.
+			if ( null == self::$instance ) {
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+		}
+	}
+}
+
+if ( ! function_exists( 'blank_plugin' ) ) {
+
+	/**
+	 * Returns instanse of the plugin class.
+	 *
+	 * @since  1.0.0
+	 * @return object
+	 */
+	function blank_plugin() {
+		return Blank_Plugin::get_instance();
+	}
+}
+
+blank_plugin();
---
+++ includes/admin/class-plugin-admin.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Sets up the admin functionality for the plugin.
+ *
+ * @package    Blank_Plugin
+ * @subpackage Admin
+ * @author     Cherry Team
+ * @license    GPL-3.0+
+ * @copyright  2002-2016, Cherry Team
+ */
+
+// If class `Blank_Plugin_Admin` doesn't exists yet.
+if ( ! class_exists( 'Blank_Plugin_Admin' ) ) {
+
+	/**
+	 * Blank_Plugin_Admin class.
+	 */
+	class Blank_Plugin_Admin {
+
+		/**
+		 * A reference to an instance of this class.
+		 *
+		 * @since 1.0.0
+		 * @var object
+		 */
+		private static $instance = null;
+
+		/**
+		 * Class constructor.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function __construct() {
+
+			// Include libraries from the `includes/admin`
+			$this->includes();
+
+			// Load the admin menu.
+			add_action( 'admin_menu', array( $this, 'menu' ) );
+
+			// Load admin stylesheets.
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+
+			// Load admin JavaScripts.
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		}
+
+		/**
+		 * Include libraries from the `includes/admin`.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function includes() {
+
+			// Include plugin pages.
+			require_once( trailingslashit( BLANK_PLUGIN_DIR ) . 'includes/admin/pages/class-plugin-main-page.php' );
+			require_once( trailingslashit( BLANK_PLUGIN_DIR ) . 'includes/admin/pages/class-plugin-options-page.php' );
+		}
+
+		/**
+		 * Register the admin menu.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 * @return void
+		 */
+		public function menu() {
+			add_menu_page(
+				esc_html__( 'Blank Plugin', 'blank-plugin' ),
+				esc_html__( 'Blank Plugin', 'blank-plugin' ),
+				'edit_theme_options',
+				'blank-plugin',
+				array( 'Blank_Plugin_Main_Page', 'get_instance' ),
+				'',
+				58
+			);
+
+			add_submenu_page(
+				'blank-plugin',
+				esc_html__( 'Options Example', 'blank-plugin' ),
+				esc_html__( 'Options Example', 'blank-plugin' ),
+				'edit_theme_options',
+				'blank-plugin-options-page',
+				array( 'Blank_Plugin_Options_Page', 'get_instance' )
+			);
+		}
+
+		/**
+		 * Enqueue admin stylesheets.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @param  string $hook The current admin page.
+		 * @return void
+		 */
+		public function enqueue_styles( $hook ) {
+			if ( Blank_Plugin_Admin::is_plugin_page() ) {
+				wp_enqueue_style(
+					'blank-plugin-admin',
+					esc_url( BLANK_PLUGIN_URI . 'assets/admin/css/min/blank-plugin-admin.min.css' ),
+					array(), BLANK_PLUGIN_VERSION,
+					'all'
+				);
+			}
+		}
+
+		/**
+		 * Enqueue admin JavaScripts.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @param  string $hook The current admin page.
+		 * @return void
+		 */
+		public function enqueue_scripts( $hook ) {
+			if ( Blank_Plugin_Admin::is_plugin_page() ) {
+				wp_enqueue_script(
+					'blank-plugin-admin',
+					esc_url( BLANK_PLUGIN_URI . 'assets/admin/js/min/blank-plugin-admin.min.js' ),
+					array( 'cherry-js-core' ),
+					BLANK_PLUGIN_VERSION,
+					true
+				);
+			}
+		}
+
+		/**
+		 * Check current plugin page.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @return bool
+		 */
+		public static function is_plugin_page() {
+			$screen = get_current_screen();
+
+			return ( ! empty( $screen->base ) && false !== strpos( $screen->base, BLANK_PLUGIN_SLUG ) ) ? true : false ;
+		}
+
+		/**
+		 * Returns the instance.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 * @return object
+		 */
+		public static function get_instance() {
+
+			// If the single instance hasn't been set, set it now.
+			if ( null == self::$instance ) {
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+		}
+	}
+
+}
+
+if ( ! function_exists( 'blank_plugin_admin' ) ) {
+
+	/**
+	 * Returns instanse of the plugin class.
+	 *
+	 * @since  1.0.0
+	 * @return object
+	 */
+	function blank_plugin_admin() {
+		return Blank_Plugin_Admin::get_instance();
+	}
+}
+
+blank_plugin_admin();
