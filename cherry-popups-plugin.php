1a51e099f6ac99e480f30375e1dc4479131a6015
shin <shin@templatemonster.me>
2016-11-16 15:31:54 +0200

    upd: version

--- cherry-popups-plugin.php
+++ cherry-popups-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cherry PopUps
  * Plugin URI:  http://www.cherryframework.com/
  * Description: A plugin for WordPress.
- * Version:     1.0.0
+ * Version:     1.0.1
  * Author:      Cherry Team
  * Text Domain: cherry-popups
  * License:     GPL-3.0+
@@ -12,7 +12,7 @@
  *
  * @package Cherry_Popups
  * @author  Cherry Team
- * @version 1.0.0
+ * @version 1.0.1
  * @license GPL-3.0+
  * @copyright  2002-2016, Cherry Team
  */
